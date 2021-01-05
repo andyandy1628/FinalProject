3aaf156080ba7914bd6724a5348c69f62050359c
andyandy1628 <76104645+andyandy1628@users.noreply.github.com>
2021-01-06 00:02:40 +0800

    January 5th, 2020, 9:40pm - 10:57pm
    (For the work session on December 16th, 2020, I meant to write 9:40 pm - 11:05 pm rather than 10:05 pm) 
    In this work session, I finished addressing another two problems. I spent a lot of time on first question to figure out the calculation of dominant offsprings. It took me a while to realize if both parents are homozygous dominant, then all of the possible offsprings would be 100% dominant. If one of the parent is homozygous dominant, then the number of offsprings containing a dominant allele equals to the number of homozygous parents times the other parents. After figuring out the calculation, the coding was not hard. I figured out to use combination to make my calculation easier.  For the second question, it took me a lot of time to write the codes. I had to search for the use of "strip" to finish the question. I also need to learn to use "open" and "read" to read the lines in the file provided by the question. I used a dictionary in order to return two corresponding results.

--- myFile.py
+++ myFile.py
@@ -1,3 +1,6 @@
+#Import math for some calculations in this file
+import math 
+
 #Question 1 DNA: Count "A","T","C","G" respectively in a DNA sequence
 def count(dnaSeq)
     print(dnaSeq.count("A"),dnaSeq.count("C"),dnaSeq.count("G"),dnaSeq.count("T"))
@@ -62,6 +65,42 @@ def translation(rnaSeq):
         codon = rnaSeq[i:i + 3] 
         protein+= RNA_codon_table[codon] 
     print(protein)
+    
+   
+#Question 6 IPRB: Calculate the probability of producing offsprings with a dominant allele
+def pr_dominant(k,m,n):
+    #total population of mating organisms
+    population = k + m + n
+    #total number of offsprings produced if any two organisms can mate
+    All_Offspring = math.comb(population, 2)
+    #total number of offsprings with dominant phenotype
+    #Any two organisms of k, any groups of one k and one m, any groups of one k and one n, half of the groups of one m and one n, 3/4 of any two organisms of m can produce offsprings with a dominant allele
+    Dominant_Offspring = math.comb(k,2) + k*m + k*n + 0.5*m*n + 0.75*(math.comb(m,2))
+    #Calculate the probability of producing offsprings with dominant phenotype
+    probability = Dominant_Offspring/All_Offspring
+    print(probability)
+
+#Question 7 GC: Select the name of the dna sequence which has the biggest gc content from a FASTA file. Calculate the biggest gc content. 
+def GC_content(data):
+    #Open the data file. Data refers to "rosalind_gc.txt", a file provided by the question
+    dnafile = open(data,"r")
+    #Read the file and get the raw data
+    rawdata = dnafile.read()
+    #Creates a dictionary
+    dic = {}
+    #The file is a fasta file, so identify  ">" to split different dna sequences from each other. Ignore every title line starting with ">"
+    for dna in rawdata.split(">")[1:]:
+        parts = dna.split("\n")
+        #The title line starting with ">"
+        dna = parts[0]
+        #Get the dna sequence connecting different lines together
+        seq = ''.join(parts[1:])
+        #Calculate the gc content
+        gc = 100 * (seq.count("G") + seq.count("C")) / float(len(seq))
+        #Use the dictionary. Key is dna name and value is the gc content of dna
+        dic[gc] = dna
+    #print out the biggest gc content and the corresponding dna name
+    print(dic[max(dic)],max(dic))
     
 
     
